3095d056cc29e59da8fcceb355de8c7cb697cb3c
nicolapasa <nicola.pasa@gmail.com>
2024-04-11 15:26:02 +0200

    fix bug2

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { ReactNode } from "react";
 import Navbar from '../navbar/Navbar'
 import style from './layout.module.css'
 import Footer from '../footer'
 
 import 'react-toastify/dist/ReactToastify.css';
-const layout = ({children}) => {
+
+type Props= {
+  children?: ReactNode
+  // any props that come into the component
+}
+
+const layout = ({children}:Props) => {
   
   return (
         <div className={style.container}>
