eb0beb66e69a60d5d8e29f11cad376a93da6d808
nicolapasa <nicola.pasa@gmail.com>
2024-04-12 08:56:09 +0200

    fix major bug

--- pages/api/post/[id]/index.tsx
+++ pages/api/post/[id]/index.tsx
@@ -1,11 +1,13 @@
 
 import { MongoClient, ObjectId, WithId } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next'  
-type ResponseData = {
-  message: string,
-  post:WithId<Document>[],
+interface ResponseData  {
+  message: string
   error: object
+  post: ObjectId[]
 }  
+
+
 
     export default async function handler( req: NextApiRequest,
       res: NextApiResponse<ResponseData>) {
@@ -19,14 +21,24 @@ type ResponseData = {
 
       switch (req.method) {
         case 'GET':
-         
+          interface Post {
+            // Define the structure of your post data here
+            // For example:
+            _id: ObjectId;
+            title: String;
+            content: String;
+            created: String;
+            category: String;
+            author: String;
+            // other properties...
+          }
      
         
           try {
           
             if( id !== 'undefined'){
             
-              const post = await db.collection('posts').find({_id: new ObjectId(id) }).toArray();
+              const post : Post[]= await db.collection<Post>('posts').find({_id: new ObjectId(id) }).toArray();
               
               res.status(200).json({post} );
     
