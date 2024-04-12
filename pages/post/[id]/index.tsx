2260f4c40c1a7e8db338e84ed266ce346b6748f7
nicolapasa <nicola.pasa@gmail.com>
2024-04-12 09:45:52 +0200

    fixed error in single post page

--- pages/post/[id]/index.tsx
+++ pages/post/[id]/index.tsx
@@ -4,10 +4,29 @@ import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import style from './index.module.css'
 import Image from 'next/image'
+
+interface Post {
+  _id: string;
+  title: string;
+  content: string;
+  image: string;
+  category: string;
+  author: string;
+  created: string;
+}
+
 const page = () => {
   const router=  useRouter()
   const id=router.query.id
-  const [post, setPost] = useState({})
+  const [post, setPost] = useState<Post>({
+    _id: '',  
+    title: '',
+    content: '',
+    image: '',
+    category: '',
+    author: '',
+    created:''
+  })
   const getSinglePost=async()=>{
     console.log(id)
      const response=await axios.get(`http://localhost:3000/api/post/${id}`)
